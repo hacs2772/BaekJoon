e7b8619debcb069301d7853724564749ef733c0f
JiHun Ha <107793142+hacs2772@users.noreply.github.com>
2023-08-30 13:57:11 +0900

    Create 18108.java

---
+++ Bronze/18108.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args){
+
+		Scanner xx = new Scanner(System.in);
+
+        int x = xx.nextInt();
+        
+        System.out.println(x - 543);
+
+        xx.close();
+        
+
+	}
+}
