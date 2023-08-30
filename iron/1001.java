9036a94c570e917b52c09a66ee93cbd9463dc5b1
JiHun Ha <107793142+hacs2772@users.noreply.github.com>
2023-08-30 13:52:40 +0900

    Create 1001.java

---
+++ iron/1001.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner xx = new Scanner(System.in);
+
+        int x = xx.nextInt();
+        int y = xx.nextInt();
+
+        System.out.print(x - y);
+
+        xx.close();
+    }
+}
