a96b0aadd15082729297d15c5087b8d681b26bd7
JiHun Ha <107793142+hacs2772@users.noreply.github.com>
2023-08-30 14:42:32 +0900

    Create 11382.java

---
+++ iron/11382.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args){
+		
+		
+		Scanner xx = new Scanner(System.in);
+
+        long x = xx.nextLong();
+        long y = xx.nextLong();
+        long z = xx.nextLong();
+        
+        System.out.println(x + y+ z);
+
+        xx.close();
+        
+
+	}
+}
