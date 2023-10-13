f4832cb6767355bfdc363d5ad1d683c3caf7fede
JiHun Ha <107793142+hacs2772@users.noreply.github.com>
2023-10-13 09:01:43 +0900

    Create 11654_아스키 코드.java

---
+++ Bronze/11654_아스키 코드.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    	Scanner xx = new Scanner(System.in);
+
+        int st = xx.next().charAt(0);
+        
+        System.out.println(st);
+        
+	}
+}
