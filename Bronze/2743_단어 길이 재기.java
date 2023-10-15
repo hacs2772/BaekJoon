cf3b3cbab9ccdb22a63af884ec98d3e7aabc54ac
JiHun Ha <107793142+hacs2772@users.noreply.github.com>
2023-10-16 06:58:55 +0900

    Create 2743_단어 길이 재기.java

---
+++ Bronze/2743_단어 길이 재기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    	Scanner xx = new Scanner(System.in);
+
+        String str = xx.next();
+        
+        System.out.println(str.length());
+        
+	}
+}
