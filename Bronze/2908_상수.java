9bdc405a82765f1af61148fe25fdc11549facf4c
JiHun Ha <107793142+hacs2772@users.noreply.github.com>
2023-10-24 09:11:46 +0900

    Update 2908_상수.java

--- Bronze/2908_상수.java
+++ Bronze/2908_상수.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) { //2908
         Scanner xx = new Scanner(System.in);
         
         int nu1 = xx.nextInt();
@@ -11,5 +11,11 @@ public class Main {
 		nu2 = Integer.parseInt(new StringBuilder().append(nu2).reverse().toString());
 		
 		System.out.println(nu1 > nu2 ? nu1 : nu2);
+		
+		/*
+		 * int result = 0; String str = String.valueOf(nu1); for (int i = str.length() -
+		 * 1; i >= 0; i--) { result = result * 10 + (str.charAt(i) - '0'); }
+		 * System.out.println("뒤집기 결과 = "+result);
+		 */
     }
 }
